ce0a59b05249115e5898c54dc7c655c62fc03b2a
vaspahomov <vas2142553@gmail.com>
2019-04-20 04:07:08 +0500

    add green screan

--- level.py
+++ level.py
@@ -3,6 +3,7 @@ import pygame
 from game_objects.rocket import Rocket
 from game_objects.hole import Hole
 from process_events import process_key_event
+from drawing import draw_items
 
 
 class Level:
@@ -10,9 +11,9 @@ class Level:
         self.rocket = Rocket(*start_coords)
         self.planets = []
         self.hole = Hole(*finish_coords)
+        self.is_game_over = False
 
     def on_tick(self, screen: pygame.display, events):
         process_key_event(self, events)
-        # ToDo вызов изменений логики
+        draw_items(self, screen)
         # ToDo вызов отрисовки
-        pass
--- process_events.py
+++ process_events.py
@@ -2,7 +2,9 @@ import pygame
 
 
 def process_key_event(level, events):
-    for event in events:
+    for event in filter(lambda x: x.type == pygame.KEYDOWN, events):
+        if event.type == pygame.QUIT:
+            level.is_game_over = True
         if event.key == pygame.K_SPACE:
             level.rocket.change_boost()
         if event.key == pygame.K_a:
