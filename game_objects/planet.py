cb22157d4845697f350e89970a2562af74b978a0
Potapov Lev <potap99122@gmail.com>
2019-04-20 03:53:15 +0500

    add Planet implementation

--- game_objects/planet.py
+++ game_objects/planet.py
@@ -1,15 +1,18 @@
 from game_objects.game_object import GameObject
 from game_objects.human import Human
+from program_variables import G
+import math
 
 
 class Planet(GameObject):
     def __init__(self, x: int, y: int, radius: int):
         super().__init__(x, y)
         self.radius = radius
+        self.gravity = G * (math.pi * self.radius * self.radius)
 
     def get_gravity(self) -> float:
         # TODO какая-то физика, которая зависит от радиуса планеты
-        pass
+        return self.gravity
 
     def get_human(self) -> Human:
         pass
--- game_objects/rocket.py
+++ game_objects/rocket.py
@@ -1,4 +1,7 @@
 from game_objects.game_object import GameObject
+from game_objects.planet import Planet
+from program_variables import boost_power
+import math
 
 
 class Rocket(GameObject):
@@ -6,9 +9,17 @@ class Rocket(GameObject):
         super().__init__(x, y)
         self.acceleration = 0.0
         self.angle = 0.0
+        self.fuel = 100
+        self.boost_power = boost_power
+        self.vx = 0
+        self.vy = 0
 
-    def change_angle(self, angle: float):
-        pass
+    def change_angle(self, delt_angle: float):
+        self.angle += delt_angle
 
-    def change_boost(self, moment: float):
-        pass
+    def enable_boost(self, moment: float):
+        self.vx += self.boost_power * math.cos(self.angle)
+        self.vy += self.boost_power * math.sin(self.angle)
+
+    # def landing_on_planet(self, closest_planet: Planet):
+    #     self.
