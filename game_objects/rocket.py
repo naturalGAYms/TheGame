eabf066983f81de1935318dd66da32d47a858ae4
vaspahomov <vas2142553@gmail.com>
2019-04-20 02:26:16 +0500

    rename RocketBase to Rocket

--- game_objects/rocket.py
+++ game_objects/rocket.py
@@ -1,7 +1,7 @@
 from game_objects.game_object import GameObject
 
 
-class RocketBase(GameObject):
+class Rocket(GameObject):
     def __init__(self, x: int, y: int):
         super().__init__(x, y)
         self.acceleration = 0.0
