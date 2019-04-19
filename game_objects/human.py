e08def26806cd3d616a943f17008aa717ea21b10
vaspahomov <vas2142553@gmail.com>
2019-04-20 02:19:26 +0500

    replace game objects in game_objects directory

--- game_objects/human.py
+++ game_objects/human.py
@@ -1,6 +1,6 @@
-from game_object import GameObject
+from game_objects.game_object import GameObject
 
 
-class Hole(GameObject):
+class Human(GameObject):
     def __init__(self, x: int, y: int):
         super().__init__(x, y)
