132da840c6d03ee0fce824da290b6e8b8b6dd9b0
Potapov Lev <potap99122@gmail.com>
2019-04-20 17:40:40 +0500

    done humans

--- game_objects/human.py
+++ game_objects/human.py
@@ -1,11 +1,11 @@
 import math
 
+
 from game_objects.game_object import GameObject
 
 
 class Human():
-    def __init__(self, angle: float, radius_of_native_planet: int):
-        # super().__init__(angle, 0)
-        self.x = math.cos(angle) * radius_of_native_planet
-        self.y = math.sin(angle) * radius_of_native_planet
+    def __init__(self, angle: float, planet_x: int, planet_y: int, radius_of_native_planet: int):
+        self.x = math.cos(angle) * radius_of_native_planet + planet_x
+        self.y = math.sin(angle) * radius_of_native_planet + planet_y
         self.angle = angle
