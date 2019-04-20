cd2df2175f1b02387c1e86956712c4ee32bcf4ff
Zeliboba <tolya12345w@gmail.com>
2019-04-20 06:53:35 +0500

    fix rotating

--- drawing.py
+++ drawing.py
@@ -15,6 +15,14 @@ def draw_items(_level, surface: pygame.display):
     surface.blit(background_image, (0, 0))
     rocket_coords = _level.rocket.get_coordinates()
     rocket_angle = - _level.rocket.return_angle() * 57.32
-    rocket_image_with_angle = pygame.transform.rotate(rocket_image, rocket_angle)
-    surface.blit(rocket_image_with_angle, rocket_coords)
+    rocket_image_with_angle = rot_center(rocket_image, rocket_angle, rocket_coords)
+    surface.blit(*rocket_image_with_angle)
     pygame.display.flip()
+
+
+def rot_center(image, angle, rocket_coords):
+    rot_image = pygame.transform.rotate(image, angle)
+    rect = image.get_rect()
+    rect.move_ip(*rocket_coords)
+    rot_rect = rot_image.get_rect(center=rect.center)
+    return rot_image, rot_rect
