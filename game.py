b38385a3b65090c763961857855611b010579bb8
vaspahomov <vas2142553@gmail.com>
2019-04-20 11:39:44 +0500

    fix levels

--- game.py
+++ game.py
@@ -2,6 +2,7 @@ from level import Level
 import pygame
 from game_objects.planet import Planet
 from game_objects.human import Human
+from game_objects.asteroid import Asteroid
 
 GAME_NAME = 'Space ships'
 SCREEN_WIDTH = 1500
@@ -11,18 +12,54 @@ SCREEN_HEIGHT = 800
 class Game:
     def __init__(self):
         self.levels = [
-            Level((50, 50), (300, 400), [
-                Planet(100, 100, 120, Human(100, 100)),
-                Planet(1000, 300, 100, Human(100, 100)),
-                Planet(500, 600, 70, Human(100, 100)),]
-                  ),
-            Level((400, 40), (600, 600), []),
-            Level((100, 400), (1000, 400), []),
-            Level((800, 40), (400, 100), []),
-            Level((10, 40), (1000, 300), []),
+            Level((50, 50), (300, 400),
+                  [
+                      Planet(100, 100, 120, Human(100, 100)),
+                      Planet(1000, 300, 100, Human(100, 100)),
+                      Planet(500, 600, 70, Human(100, 100)), ],
+                  [
+                      Asteroid(800, 400),
+                      Asteroid(200, 100)
+                  ]),
+            Level((400, 40), (600, 600),
+                  [
+                      Planet(100, 100, 120, Human(100, 100)),
+                      Planet(1000, 300, 100, Human(100, 100)),
+                      Planet(500, 600, 70, Human(100, 100)), ],
+                  [
+                      Asteroid(800, 400),
+                      Asteroid(200, 100)
+                  ]),
+            Level((100, 400), (1000, 400),
+                  [
+                      Planet(100, 100, 120, Human(100, 100)),
+                      Planet(1000, 300, 100, Human(100, 100)),
+                      Planet(500, 600, 70, Human(100, 100)), ],
+                  [
+                      Asteroid(800, 400),
+                      Asteroid(200, 100)
+                  ]),
+            Level((800, 40), (400, 100),
+                  [
+                      Planet(100, 100, 120, Human(100, 100)),
+                      Planet(1000, 300, 100, Human(100, 100)),
+                      Planet(500, 600, 70, Human(100, 100)), ],
+                  [
+                      Asteroid(800, 400),
+                      Asteroid(200, 100)
+                  ]),
+            Level((10, 40), (1000, 300),
+                  [
+                      Planet(100, 100, 120, Human(100, 100)),
+                      Planet(1000, 300, 100, Human(100, 100)),
+                      Planet(500, 600, 70, Human(100, 100)), ],
+                  [
+                      Asteroid(800, 400),
+                      Asteroid(200, 100)
+                  ]),
         ]
         self.index = 0
-        self.current_level: Level = Level((0, 0), (100, 100), [])
+        self.current_level: Level = Level((0, 0), (100, 100), [], [])
         self.screen: pygame.display = None
         # size = [SCREEN_WIDTH, SCREEN_HEIGHT]
         self.surface = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
